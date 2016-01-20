61923d0985c488223eba7bdb9edc970853e3376b
Volodymyr K <vkuzbida@n-ix.com>
2016-01-20 17:22:02 +0200

    add result state

--- js/Game.js
+++ js/Game.js
@@ -2,6 +2,18 @@ DesireDesk.Game = function (game) {
     this.leftImage;
     this.rightImage;
     this.imageSetNum = 1;
+    this.step = 1;
+    this.checkedImages = {};
+    this.newImages = [
+        {name: 'image1', src: 'assets/images/test/1.jpg'},
+        {name: 'image2', src: 'assets/images/test/2.jpg'},
+        {name: 'image3', src: 'assets/images/test/3.jpg'},
+        {name: 'image4', src: 'assets/images/test/4.jpg'},
+        {name: 'image5', src: 'assets/images/test/5.jpg'},
+        {name: 'image6', src: 'assets/images/test/6.jpg'},
+        {name: 'image7', src: 'assets/images/test/7.jpg'},
+        {name: 'image8', src: 'assets/images/test/8.jpg'}
+    ];
 };
 
 DesireDesk.Game.prototype = {
@@ -9,19 +21,19 @@ DesireDesk.Game.prototype = {
         this.startBG = this.add.image(0, 0, 'bg');
         this.startBG.width = this.world.width;
         this.startBG.height = this.world.height;
+        this.preloadBar = this.add.sprite(this.world.centerX, this.world.centerY, 'preloaderBar');
+        this.preloadBar.anchor.setTo(0.5,0.5);
+        this.load.setPreloadSprite(this.preloadBar);
 
+        //preloading images set
+        for(var i = 0; i < this.newImages.length; i++){
+            this.load.image(this.newImages[i].name, this.newImages[i].src);
+        }
 
-        this.load.image('image1', 'assets/images/test/1.jpg');
-        this.load.image('image2', 'assets/images/test/2.jpg');
-        this.load.image('image3', 'assets/images/test/3.jpg');
-        this.load.image('image4', 'assets/images/test/4.jpg');
-        this.load.image('image5', 'assets/images/test/5.jpg');
-        this.load.image('image6', 'assets/images/test/6.jpg');
-        this.load.image('image7', 'assets/images/test/7.jpg');
-        this.load.image('image8', 'assets/images/test/8.jpg');
     },
 
     create: function () {
+        this.preloadBar.destroy();
         this.textBelow = this.add.text(this.world.centerX - 100, this.world.height - 100, 'Make Your Choise', {fill: '#fff'});
         this.titleText = this.add.text(this.world.centerX - 100, 30, 'Board of Dream', {fill: '#fff', size: 50});
 
@@ -38,10 +50,17 @@ DesireDesk.Game.prototype = {
         this.rightImage = this.add.image(this.world.centerX + 100, 200, 'image' + (this.imageSetNum+1));
         this.rightImage.width = this.world.width / 3;
         this.rightImage.height = this.world.height / 3;
+        this.rightImage.inputEnabled = true;
+        this.rightImage.events.onInputDown.add(this.chooseImg, this);
     },
 
     chooseImg: function (text, pointer) {
-        this.imageSetNum++;
-      console.log(text.key);
+        this.checkedImages[this.step+'_step'] = text.key;
+        console.log(this.checkedImages);
+        this.step++;
+        this.imageSetNum = this.step + 1;
+        if(this.step === 5) {
+            this.state.start('Result', true, false, this.checkedImages);
+        }
     }
 };
---
+++ js/Result.js
@@ -0,0 +1,76 @@
+DesireDesk.Result = function(){
+    this.startBG;
+    this.titleText;
+    this.menuGroup;
+    this.checkedImages;
+
+    var _this = this;
+};
+
+DesireDesk.Result.prototype = {
+    init: function(checkedImages) {
+        this.checkedImages = checkedImages;
+        console.log(checkedImages)
+    },
+
+    create: function () {
+        this.startBG = this.add.image(0, 0, 'bg');
+        this.startBG.width = this.world.width;
+        this.startBG.height = this.world.height;
+
+        this.titleText = this.add.bitmapText(this.world.centerX - 120, 30, 'eightbitwonder', 'Your result', 24);
+
+
+        //*****menu btn*****
+        var menuButton = this.add.button(this.world._width - 50, 50, "menu", this.toggleMenu, this);
+        menuButton.anchor.set(0.5);
+        menuButton.width = 70;
+        menuButton.height = 70;
+
+        //*****menu list*****
+        this.menuGroup = this.add.group();
+        this.menuGroup.enableBody = true;
+        this.menuGroup.stage.backgroundColor = '#171642';
+        this.menuGroup.stage.width = 200;
+        this.menuGroup.stage.height = this.world._height;
+        this.menuGroup.z = 1;
+
+        var menuHelp = this.add.text(this.world._width + 100, 200, 'Help', { font: "32px Arial", fill: '#3F51B5'});
+        menuHelp.inputEnabled = true;
+        menuHelp.events.onInputDown.add(this.goTo, {stateToGo: 'Help'});
+        this.menuGroup.add(menuHelp);
+
+        var menuFriends = this.add.text(this.world._width + 100, 250, 'Friends', { font: "32px Arial", fill: '#3F51B5'});
+        menuFriends.inputEnabled = true;
+        menuFriends.events.onInputDown.add(this.goTo, {stateToGo: 'Friends'});
+        this.menuGroup.add(menuFriends);
+
+        var menuSupport = this.add.text(this.world._width + 100, 350, 'Support', { font: "32px Arial", fill: '#3F51B5'});
+        menuSupport.inputEnabled = true;
+        menuSupport.events.onInputDown.add(this.goTo, {stateToGo: 'Support'});
+        this.menuGroup.add(menuSupport);
+
+        var menuWhy = this.add.text(this.world._width + 100, 400, 'Why', { font: "32px Arial", fill: '#3F51B5'});
+        menuWhy.inputEnabled = true;
+        menuWhy.events.onInputDown.add(this.goTo, {stateToGo: 'Why'});
+        this.menuGroup.add(menuWhy);
+
+    },
+
+    toggleMenu: function () {
+        if(this.menuGroup.x == 0){
+            var menuTween = this.add.tween(this.menuGroup).to({
+                x: -250
+            }, 500, Phaser.Easing.Bounce.Out, true);
+        }
+        if(this.menuGroup.x == -250){
+            var menuTween = this.add.tween(this.menuGroup).to({
+                x: 0
+            }, 500, Phaser.Easing.Bounce.Out, true);
+        }
+    },
+
+    goTo: function () {
+        stateService.start(this.stateToGo);
+    }
+};
